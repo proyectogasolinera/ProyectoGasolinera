4ba1623322f82e3a3dc381281bbe3e2922adccd3
delva <delva@LAPTOP-8OLE87BL>
2019-05-02 14:51:37 +0200

    ALejandro del val: modificacion (insert) Administrador

--- ProyectoGasolinera/src/bd/BD_Administrador.java
+++ ProyectoGasolinera/src/bd/BD_Administrador.java
@@ -9,14 +9,38 @@ import java.sql.*;
 import java.util.*;
 import modelo.Administrador;
 
-
 import java.time.LocalDate;
 
 public class BD_Administrador extends BD_Conector{
 	private static Statement s;	
 	private static ResultSet reg;
+	
+	
+	public int add_admin(Administrador admin){	
+		String cadenaSQL="INSERT INTO administrador VALUES('" + admin.getId_admin() + "','" +
+		admin.getNombre_admin()+"','"+ admin.getPassword() +"','"+ admin.getDni()+"','"+
+				admin.getCorreo()+"','"+admin.getTelefono()+"','"+admin.getDireccion()+"','"+
+				admin.getCodPostal()+"','"+admin.getFechaAlt()+"')";
+		
+		try{
+			//Si las filas retorna 1 el usuario ha sido a馻dido, si devuelve 0, el usuario no se a馻dio, si devuelve -1 no se a馻de por algun error de BD 
+			this.abrir();
+			s=c.createStatement();
+			int filas=s.executeUpdate(cadenaSQL);
+		
+			s.close();
+			this.cerrar();
+			return filas;
+			
+			}
+			catch ( SQLException e){			
+				return -1;
+			}
+	
+	}
 
 
+//Select
 public  Vector<Administrador> selectAdmin(String dni){
 	String cadenaSQL="SELECT * from administrador WHERE dni='"+dni+"'";
 	Vector<Administrador> listadoAdmin=new Vector<Administrador>();
--- ProyectoGasolinera/src/principal/Main.java
+++ ProyectoGasolinera/src/principal/Main.java
@@ -6,7 +6,8 @@ package principal;
  */
 import modelo.*;
 import bd.*;
-
+import java.time.format.DateTimeFormatter;
+import java.time.format.*;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -17,17 +18,19 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
 		BD_Conector.BD_Ini("aldautomotivebbdd");
+		//DateTimeFormatter fechaFormateada = DateTimeFormatter.ofPattern("yyyy-LL-dd");
 		BD_Administrador bdA= new BD_Administrador();
 		int op;
-		String dni_adm;
+		
 		String id_admin;
-		String Nombre_admin;
-		String Password;
+		String nombre_admin;
+		String password;
+		String dni_adm;
 		String correo;
 		String telefono;
 		String direccion;
 		String codPostal;
-		LocalDate fechaAlt;
+
 		
 		
 		do{
@@ -43,6 +46,42 @@ public class Main {
 					System.out.println(listaAdmin.get(i).toString());
 				break;
 			case 2:
+				sc.nextLine();
+				System.out.println("Introducce datos nuevo Administrador");
+				System.out.println("*******************");
+				System.out.println("Identificador administrado: ");
+				id_admin=sc.nextLine();
+				System.out.println("Nombre:");
+				nombre_admin=sc.nextLine();
+				System.out.println("Password:");
+				password=sc.nextLine();
+				System.out.println("dni:");
+				 dni_adm=sc.nextLine();
+				 System.out.println("Correo electronico:");
+				 correo=sc.nextLine();
+				 System.out.println("Telefono:");
+				 telefono=sc.nextLine();
+				 System.out.println("dirrecion:");
+				 direccion=sc.nextLine();
+				 System.out.println("Codigo postal:");
+				 codPostal=sc.nextLine();	 
+				 LocalDate fechaAlt=LocalDate.now();
+				 
+				 Administrador admin=new Administrador(id_admin,nombre_admin,password,dni_adm,correo,telefono,direccion,codPostal,fechaAlt);
+				 int filas=bdA.add_admin(admin);
+				 if(filas==1){
+						System.out.println("Administrador nuevo introduccido");
+				 		System.out.println(admin.toString());
+				 }
+					else
+						System.out.println("Error - "+filas);
+					
+					break;
+				 
+			case 3:
+				break;
+			
+				
 				
 
 			
