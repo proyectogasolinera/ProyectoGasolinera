111af186a6658863698ca4668443a5b610500402
admin <admin@Win7_1TB>
2019-05-13 09:30:03 +0200

    Alejandro del val: introdducion selects modificaciones

--- ProyectoGasolinera/src/bd/BD_Modificacion.java
+++ ProyectoGasolinera/src/bd/BD_Modificacion.java
@@ -14,9 +14,80 @@ import java.time.LocalDate;
 public class BD_Modificacion extends BD_Conector{
 	private static Statement s;	
 	private static ResultSet reg;
+	private String cadenaSQL;
 	
 //selects
-	
+	//Select donde recibe como parametro 1 campo de tipo String 
+		public  Vector<Modificacion> selectModificacion(String dato , String campo){
+			cadenaSQL="SELECT * from modificaciones WHERE "+campo+" ='"+dato+"'";
+			System.out.println(cadenaSQL);
+			Vector<Modificacion> listadoModificaciones=new Vector<Modificacion>();
+			try{
+				this.abrir();
+				s=c.createStatement();
+				reg=s.executeQuery(cadenaSQL);
+				
+				while ( reg.next()){
+					java.sql.Date f=reg.getDate("fecha");
+					LocalDate fBuena=f.toLocalDate();
+					listadoModificaciones.add(new Modificacion (reg.getInt(1),reg.getString(2),fBuena,reg.getString(4),reg.getInt(5)));
+				}
+				s.close();
+				this.cerrar();
+				return listadoModificaciones;
+			}
+			catch ( SQLException e){		
+				return null;			
+			}
+		}
+		//select donde recibe como pararamtro 2 campos 1 de tipo String y otro de tipo int
+		
+		public  Vector<Modificacion> selectModificacion(String dato1,int dato2 , String campo1,String campo2){
+			String cadenaSQL="SELECT * from modificaciones WHERE "+campo1+"  ='"+dato1+"' and "+campo2+"  ='"+dato2+"'";
+
+			Vector<Modificacion> listadoModificaciones=new Vector<Modificacion>();
+			try{
+				this.abrir();
+				s=c.createStatement();
+				reg=s.executeQuery(cadenaSQL);
+				
+				while ( reg.next()){
+					java.sql.Date f=reg.getDate("fecha");
+					LocalDate fBuena=f.toLocalDate();
+					listadoModificaciones.add(new Modificacion (reg.getInt(1),reg.getString(2),fBuena,reg.getString(4),reg.getInt(5)));
+				}
+				s.close();
+				this.cerrar();
+				return listadoModificaciones;
+			}
+			catch ( SQLException e){		
+				return null;			
+			}
+		}
+		
+		// select donde recibe como parametro un campo de tipo entero
+		public  Vector<Modificacion> selectModicaciones(int dato , String campo){
+			String cadenaSQL="SELECT * from modificaciones WHERE "+campo+" ='"+dato+"'";
+
+			Vector<Modificacion> listadoModificaciones=new Vector<Modificacion>();
+			try{
+				this.abrir();
+				s=c.createStatement();
+				reg=s.executeQuery(cadenaSQL);
+				
+				while ( reg.next()){
+					java.sql.Date f=reg.getDate("fecha");
+					LocalDate fBuena=f.toLocalDate();
+					listadoModificaciones.add(new Modificacion (reg.getInt(1),reg.getString(2),fBuena,reg.getString(4),reg.getInt(5)));
+				}
+				s.close();
+				this.cerrar();
+				return listadoModificaciones;
+			}
+			catch ( SQLException e){		
+				return null;			
+			}
+		}
 
 //INSERT
 	public int add_modificacion(Modificacion mod){	
